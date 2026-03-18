f3d3d2943fef0e3ef8a7f99a2cbe53fec77a3179
Arshneer <your-email@gmail.com>
2026-03-18 19:14:03 +0530

    Initial commit - JWT Banking API

--- server.js
+++ server.js
@@ -8,12 +8,19 @@ const accountRoutes = require("./routes/accountRoutes");
 const app = express();
 
 app.use(express.json());
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"));
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Routes
 app.use("/api/auth",authRoutes);
 app.use("/api/account",accountRoutes);
 
-app.listen(5000,()=>{
- console.log("Server running on port 5000");
+// FIXED PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+ console.log(`Server running on port ${PORT}`);
 });
