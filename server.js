626e1586b8ea945b9133a87b57d2e1ee08355bd9
Arshneer <your-email@gmail.com>
2026-03-18 19:29:46 +0530

    Added home route

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const accountRoutes = require("./routes/accountRoutes");
 const app = express();
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("🚀 Banking JWT API is running...");
-});
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("🚀 Banking JWT API is running...");
+});
 // Routes
 app.use("/api/auth",authRoutes);
 app.use("/api/account",accountRoutes);
